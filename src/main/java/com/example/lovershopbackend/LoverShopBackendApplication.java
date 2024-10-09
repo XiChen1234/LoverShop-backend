9c26736f3569e491289db05d6d97e20885c22831
Xi_Chen <xichen888.private@gmail.com>
2024-10-10 00:36:11 +0800

    init: init the project

---
+++ src/main/java/com/example/lovershopbackend/LoverShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lovershopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoverShopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoverShopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lovershopbackend/LoverShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lovershopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoverShopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
